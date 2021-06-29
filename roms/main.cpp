ab13085e41a32ff5f12dcdf34b5e5de184a1880d
ronsh909 <ronsh909@gmail.com>
2021-06-29 19:27:42 +0300

    Added initial version - requires debugging

---
+++ roms/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <filesystem>
+#include <string>
+#include "chip8.h"
+
+int main(int argc, char **argv)
+{
+    std::cout << "hey" << std::endl;
+	if (argc != 2)
+	{
+        std::cerr << "[Usage]:" << argv[0] << " <ROM file>." << std::endl;
+        exit(EXIT_FAILURE);
+	}
+    else if(!std::filesystem::exists(std::string(argv[1])))
+    {
+        std::cerr << "[Path]:" << argv[1] << " does not exist." << std::endl;
+        exit(EXIT_FAILURE);
+    }
+    Chip8 vm{ argv[1] };
+    vm.run();
+   return 0;
+}
+
