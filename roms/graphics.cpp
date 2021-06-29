1922ae7863c5537a44fbf7d806923322aa26a1e0
ronsh909 <ronsh909@gmail.com>
2021-06-29 19:32:36 +0300

    Revert "Added initial version - requires debugging"
    This reverts commit ab13085e41a32ff5f12dcdf34b5e5de184a1880d.

--- roms/graphics.cpp
+++
@@ -1,8 +0,0 @@
-#include "graphics.h"
-
-Graphics::Graphics(const uint8_t width, const uint8_t height, const uint8_t scale_factor, const std::string& prog) :
-	scale_factor_(scale_factor), window_(sf::VideoMode(width * scale_factor_, height * scale_factor_), prog)
-{
-	auto desk { sf::VideoMode::getDesktopMode() };  
-	window_.setPosition(sf::Vector2i(desk.width / 4, desk.height / 4));
-};
